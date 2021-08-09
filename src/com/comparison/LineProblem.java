e3b1c8c483beea1d3346f647f1a5ebc04512183a
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-09 14:12:53 +0530

    [kshitija] : two lines are compared

--- src/com/comparison/LineProblem.java
+++ src/com/comparison/LineProblem.java
@@ -12,6 +12,27 @@ public class LineProblem {
 		int a1=12,a2=6,b1=12,b2=5;
 		double length2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
 		System.out.println(length2);
+		
+		Double d1=new Double(length1);
+	    Double d2=new Double(length2);
+	    String l1=d1.toString();
+	    String l2=d1.toString();
+	    
+		if(l2.equals(l1)) {
+			System.out.println("equal");
+		}else {
+			System.out.println(" not equal");
+		}
+		
+		System.out.println(l1.compareTo(l2));
+		if(length1>length2) {
+			System.out.println("length1 is greater than length2 ");
+		}else if(length1<length2){
+			System.out.println("length1 is smaller than length2 ");
+		}else {
+			System.out.println("length1 equals to length2 ");
+		}
+		
 	}
 
 }
