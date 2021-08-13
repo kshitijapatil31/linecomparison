f5b2cc205e0c6afec3ff61bc120fbd1c56dac7fb
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-13 22:59:23 +0530

    [kshitija]:usecase 4 solved

---
+++ src/com/comparison/LineComparison.java
@@ -0,0 +1,12 @@
+package com.comparison;
+
+class LineComparison {
+
+	public static void main(String[] args) {
+		LineProblem line=new LineProblem();
+		line.checkEquality();
+
+		
+	}
+
+}
--- src/com/comparison/LineProblem.java
+++ src/com/comparison/LineProblem.java
@@ -1,17 +1,31 @@
 package com.comparison;
 
 public class LineProblem {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("Welcome to Line Comparison");
-       
-       int x1=5,x2=9,y1=5,y2=9;
-		double length1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		System.out.println(length1);
-		int a1=12,a2=6,b1=12,b2=5;
-		double length2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
-		System.out.println(length2);
+	
+	int x1=5,x2=9,y1=5,y2=9;
+	double length1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	
+	int a1=6,a2=9,b1=6,b2=9;
+	double length2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+	
+	public void checkEquality() {
+		
+		 Double d1=new Double(length1);
+		    Double d2=new Double(length2);
+		    String l1=d1.toString();
+		    String l2=d2.toString();
+		    
+		    if(l2.equals(l1)) {
+				System.out.println("equal");
+			}else {
+				System.out.println(" not equal");
+			}
+		    System.out.println(l1.compareTo(l2));
+		}
+	
+	
+		
+		
+		
 	}
 
-}
